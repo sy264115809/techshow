04df177c74818abfb0811e80d86933aa58dd58f8
sy264115809 <264115809@qq.com>
2016-01-01 14:21:42 +0800

    remove unuse gunicorn code

--- manage.py
+++ manage.py
@@ -11,7 +11,6 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-manager.add_command('gunicorn', GunicornServer())
 
 if __name__ == '__main__':
     manager.run()
