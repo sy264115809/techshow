91b4b8a1083c7c0caf6525171861a5ca402155c1
sy264115809 <264115809@qq.com>
2016-01-12 10:41:04 +0800

    add create dev&test db sql

--- init.sql
+++ init.sql
@@ -1,1 +1,9 @@
-CREATE SCHEMA `techshow` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA `techshow`
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+CREATE SCHEMA `techshow_dev`
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+CREATE SCHEMA `techshow_tes`
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
