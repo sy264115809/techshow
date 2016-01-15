a65cb52ce8a7af357425090f4df71e89c95ec14b
sy264115809 <264115809@qq.com>
2016-01-15 10:27:30 +0800

    modify gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -22,7 +22,7 @@ graceful_timeout = 30
 
 loglevel = 'info'
 
-reload = True
+reload = False
 debug = False
 
 bind = "%s:%s" % ("127.0.0.1", 8000)
@@ -30,4 +30,4 @@ pidfile = '%s/run/%s.pid' % (path_of_current_dir, _file_name)
 # errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
 # accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
 accesslog = '-'
-access_log_format = "%(p)s %(h)s %(l)s %(u)s %(t)s “%(r)s” %(s)s %(b)s “%(f)s” “%(a)s”"
+access_log_format = "%(p)s %(h)s %(l)s %(u)s %(t)s “%(r)s” %(s)s %(b)s “referer:%(f)s” “ua:%(a)s” elapse:%(D)sμs"
